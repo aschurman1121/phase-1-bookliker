25b4b574960f1f92f29e7a50d9354afa98131400
alexandra <alexandraschurman@gmail.com>
2022-01-20 15:52:48 -0800

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+
+const bookData = 'http://localhost:3000/books'
+
+const ulForBooks = document.getElementById('list')
+
+
+
+fetch(bookData)
+.then((res) => res.json())
+.then(books => {
+    books.forEach(book => {book.title
+    let childList = document.createElement('li')
+    childList.innerText = book.title
+    ulForBooks.appendChild(childList)
+    })
+})
+
+
+
+
+});
