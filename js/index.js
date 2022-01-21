b61052503be12c7ad42c251b1c31bd594063845b
Eric Einerson <eaeiner.gmail.co>
2022-01-20 18:16:59 -0600

    Second Try

--- js/index.js
+++ js/index.js
@@ -9,10 +9,16 @@ const ulForBooks = document.getElementById('list')
 fetch(bookData)
 .then((res) => res.json())
 .then(books => {
+    console.log(books)
     books.forEach(book => {book.title
     let childList = document.createElement('li')
     childList.innerText = book.title
     ulForBooks.appendChild(childList)
+console.log(childList)
+const showDetails = document.getElementById("show-panel")
+childList.addEventListener("click", (e) => {
+    console.log(e)
+})
     })
 })
 
